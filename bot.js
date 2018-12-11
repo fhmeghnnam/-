9b5db7a9319e713112fdb552d2766b5d5d386fbe
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2018-12-11 22:33:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = [`${client.guilds.size} Server`,'!help','Type !help',`${client.users.size} Members`,'!inv','By: Mohamed | MohamedSamaYT'];
+    var setGame = [`DTM system`,'!help','Type !help',`${client.users.size} Members`,'DTM','By: Legend_FH'];
     var i = -1;
     var j = 0;
     setInterval(function (){
