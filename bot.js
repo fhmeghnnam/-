e99db231e74a7f1ac764cc5ad34efad7ad188b9f
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2018-12-14 11:13:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -415,7 +415,7 @@ function lol()
 {     client.guilds.get('476398959149973505')
 .roles.find("name", "ツ Legend")
 .setColor("RANDOM");   };   
-setInterval(lol, 100000); 
+setInterval(lol, 10000); 
 }
 );
 
