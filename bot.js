8493d2c09744d73cc63dd82ecbdebd9a5eab4567
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2018-12-15 14:47:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -703,6 +703,43 @@ if(!message.channel.guild) return message.channel.send('**هذا الأمر فق
 
 
 });
+
+
+
+client.on('message', message => {
+    var prefix = "$$";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**!bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+       var bc = new Discord.RichEmbed()
+       .addField( `${message.guild.name}`)
+       .addField('From : ', `${message.author.username}#${message.author.avatar}`)
+       .addField(' Message ', args)
+       .setColor('#ff0000')
+       m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
+
+
+
+
+
+
+
+
 client.on("message", message => {
 
           if(!message.channel.guild) return;
